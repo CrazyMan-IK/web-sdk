90d4efc5bd0cd18da46e346565253e63d53c254a
Даниэль Давлетшин <superdanik2004@gmail.com>
2023-09-05 14:14:01 +0500

    Trying use index.ts instead index.js

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import SDK from './sdk';
-import Localization, { Locale } from './localization';
-import LocalizedString from './localization/localized-string';
-export default SDK;
-export { Localization, Locale, LocalizedString };
