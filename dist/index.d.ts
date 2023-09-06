cc2ab51b8f8401120cd590da514288e24f38ba3b
Даниэль Давлетшин <superdanik2004@gmail.com>
2023-09-06 14:58:27 +0500

    Rebuild

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,9 @@
 import SDK from './sdk';
 import Localization, { Locale } from './localization';
 import LocalizedString from './localization/localized-string';
-import { musicPlayer } from './audio/music-player';
+import MusicPlayer, { musicPlayer } from './audio/music-player';
 import Timer from './timer';
+import Scaler from './scaler';
 export default SDK;
-export { Localization, Locale, LocalizedString, musicPlayer, Timer };
+export { Localization, Locale, LocalizedString, MusicPlayer, musicPlayer, Timer, Scaler };
 export * from './game-pause';
-export * from './scaler';
