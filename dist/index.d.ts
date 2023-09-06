4e3308f49c003cecee00cd10ff23748b97e3c340
Даниэль Давлетшин <superdanik2004@gmail.com>
2023-09-06 13:40:09 +0500

    Added some new exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,9 @@
 import SDK from './sdk';
 import Localization, { Locale } from './localization';
 import LocalizedString from './localization/localized-string';
+import { musicPlayer } from './audio/music-player';
+import Timer from './timer';
 export default SDK;
-export { Localization, Locale, LocalizedString };
+export { Localization, Locale, LocalizedString, musicPlayer, Timer };
+export * from './game-pause';
+export * from './scaler';
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 import SDK from './sdk';
 import Localization, { Locale } from './localization';
 import LocalizedString from './localization/localized-string';
+import { musicPlayer } from './audio/music-player';
+import Timer from './timer';
 export default SDK;
-export { Localization, Locale, LocalizedString };
+export { Localization, Locale, LocalizedString, musicPlayer, Timer };
+export * from './game-pause';
+export * from './scaler';
 //# sourceMappingURL=index.js.map
